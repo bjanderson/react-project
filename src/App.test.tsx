1c7db053c8a115fad01102df1122a0e66bfab033
BJ Anderson <andersonb79@gmail.com>
2022-09-09 22:46:22 -0400

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { App, WrappedApp } from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    render(<WrappedApp />);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello World');
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/not-there']}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Not Found');
+  });
+});
